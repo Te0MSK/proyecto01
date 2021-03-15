4740f3ee59b57ff6fc7d54b56630b7efdf0dcfee
Mateo Ortiz J <Te0j16@gmail.com>
2021-03-15 18:58:38 -0500

    Commit 2: Agregando Archivo pagina2.php con algo de concatenacion

---
+++ pagina2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<title>PROYECTO HEROKU</title>
+<meta charset="utf.8"/>
+<link rel="stylesheet" content="text/css" href="css/normalize.css" />
+<link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+
+<body>
+
+<?php   
+
+$a = 12;
+$b = 1000;
+
+$a .= $b;
+
+echo"<br   /> Concatenado $a  ."
+
+?>
+
+
+</body>
+
+</html>
