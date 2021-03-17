f7a5d6bff1c306befba611cdfe7958eefba76037
Mateo Ortiz J <Te0j16@gmail.com>
2021-03-17 09:41:06 -0500

    cambios hasta el cuarto video

---
+++ Car.php
@@ -0,0 +1,56 @@
+<?php
+class Car{
+
+public $empresa;
+public $color = "rojo";
+public $tieneCapota = true;
+public $contenidoTanque;
+private $modelo;
+
+
+private $modelos_permitidos = array("audi","mazda","mercedez","lamborghini");
+
+public function pitar(){
+    return "beep";
+}
+
+public function pedirRevision(){
+    echo "<br/>Soy un carro de marca: " . $this->empresa . " necesito una revision   
+    <br/> Necesito repintar mi color: ".$this->color. " " .$this ->pitar();
+}
+
+public function levantarCapota(){
+    if($this-> tieneCapota) echo"<br/>Levantando la capota";
+        else echo "<br/>No tengo capota para levantar";
+    
+}
+//agrega galones de gasolina
+public function llenar($cantidad){
+    $this-> contenidoTanque+= $cantidad;
+    return $this;
+
+}
+//extrae galones de gasolina cuz he can move
+public function correr($kilometros){
+    $galonesConsumidos = $kilometros/50;
+    $this-> contenidoTanque-=$galonesConsumidos;
+    return $this;
+}
+
+public function getModelo(){
+
+return "el modelo del carro es  ".$this -> modelo;
+
+
+}
+
+public function setModelo($mod){
+
+
+    if(in_array($mod, $this->modelos_permitidos)) $this -> modelo = $mod;
+    else $this -> modelo = "sin definir";
+
+
+}
+
+}
--- index.php
+++ index.php
@@ -1,43 +1,78 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-<title>PROYECTO HEROKU</title>
-<meta charset="utf.8"/>
-<link rel="stylesheet" content="text/css" href="css/normalize.css" />
-<link rel="stylesheet" content="text/css" href="css/estilos.css" />
+   <title>Clases y objetos</title>
+  <meta charset ="utf-8"
+  <meta name="viewport" content="width=device-width , initial-scale=1 , maximum-scale=1" />
+  <link rel="stylesheet" type="text/css" href="css/normalize.css"/>
+  <link rel="stylesheet" type="text/css" href="css/estilos.css"/>
 </head>
-
 <body>
+ <?php
+require 'Car.php';
 
-<div id="envoltura">
-<header>
-    <h1>Programaci&oacute;n Web</h1>
-</header>
-<div id="main"> 
-    <p>
-    
-    En este curso aprender&aacute;s a programar p&aacute;ginas web usando git.
-    <?php 
-    
-    
-  $curso = "Construccion de elementos de software 2 ";
-  echo"<br /> <br /> ESTAS EN EL CURSO: $curso ."
-    
-    
-    
-    ?>
-    </p>
-</div>
+ ?>
+ <?php
+
+ $bmw = new Car(); //instanciuacion
+ $mercedes = new Car();
+ $audi = new Car();
+
+echo "Color del bmw: $bmw->color";
+echo "<br/>";
+echo "Color del mercedes: $mercedes->color";
+
+$bmw -> color ="azul";
+$bmw ->empresa = "BMW";
+
+$mercedes -> color ="negro";
+$mercedes ->empresa = "Mercedez Benz";
+
+echo "<br/>";
+echo "Color del bmw: $bmw->color compania: $bmw->empresa";
+echo "<br/>";
+echo "Color del mercedes: $mercedes->color compania: $mercedes->empresa";
+echo "<br/>";
+echo "Bmw pitando: " .$bmw->pitar();
+echo "<br/>";
+echo "Mercedez pitando: " .$mercedes->pitar();
+
+echo "<br/><br/> Probando el uso de la variable this";
+
+$carro1 = new Car;
+$carro2 = new Car();
+
+$carro1->tieneCapota=false;
+$carro1->empresa="Toyota";
+$carro1->color="Verde";
 
 
+$carro2->empresa="Renault";
+$carro2->color="Blanco";
+
+$carro1->pedirRevision();
+$carro1->levantarCapota();
+
+echo "<br/><br/>";
+$carro2 ->pedirRevision();
+$carro2 ->levantarCapota();
+echo "<br/><br/> Usando el encadenamiento";
+$carro3= new Car();
+$gasolina = $carro3 -> llenar(10) -> correr(40) -> contenidoTanque;
+
+echo "<br/>El numero de galones que quedan en el tanque son : ". $gasolina . "galones";
+
+
+
+echo "<br /<br /> Accediendo a un atributo publico";
+
+$carro3 ->setModelo ("audi") ;
+
+//$carro3 -> modelo = "Audi";
+
+
+echo "<br/> Carro  \$3 tiene el siguiente modelo: ".$carro3 -> getModelo();
+
+ ?>
 </body>
-
-
-
-
-
-
-
-</html>
-
+    </html>
